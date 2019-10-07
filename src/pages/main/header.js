2c0211ec164f0aab619ab7bdcd7fff14ed352c6b
srfnd <fndsantos23@gmail.com>
2019-10-07 16:01:41 -0400

    first commit

---
+++ src/pages/main/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo center">gitlist</a>
+          <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <li><a href="sass.html">view the source code</a></li>
+         </ul>
+        </div>
+      </nav>
+    )
+};
+
+export default Header;
---
+++ src/pages/main/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import Axios from 'axios';
+
+
+export default class Main extends Component {
+    state = {
+        users: [],
+
+
+    }
+    componentDidMount(){
+        this.loadRepo();
+    }
+
+    loadRepo = async () => {
+        const response = await api.get('/users');
+        this.setState({ users: response.data});
+    }
+
+    render(){
+        return (
+            <div className="users-list">
+                 <ul className="collection">
+                        {this.state.users.map(users => (
+                             <li className="collection-item avatar">
+                                  <img src={users.avatar_url} alt="" class="circle"></img>
+                                  <span class="title">{users.login}</span>
+                                  <a href="{users.url}" className="secondary-content">
+                                      <i className="material-icons">grade</i>
+                                  </a>
+                             </li>
+                        ))};
+                 </ul>
+            </div>
+        )
+    }
+};
+
+
